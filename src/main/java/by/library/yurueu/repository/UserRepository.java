ba18576c683d8523e5f108cdcb0b093ba12a00e5
Dobrowydka <andrikchannelyou@gmail.com>
2022-03-06 20:38:50 +0300

    developing repositories

--- src/main/java/by/library/yurueu/repository/UserRepository.java
+++ src/main/java/by/library/yurueu/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package by.library.yurueu.repository;
 
+import by.library.yurueu.entity.Order;
+import by.library.yurueu.entity.Role;
 import by.library.yurueu.entity.User;
 
-public interface UserRepository extends BaseRepository<User> {
+import java.util.Set;
 
+public interface UserRepository extends BaseRepository<User> {
+    Set<Role> findRolesByUserId(Long userId);
+    Set<Order> findOrdersByUserId(Long userId);
+    Set<Role> findRolesByRolesId(Set<Long> rolesId);
 }
