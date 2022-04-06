6f5dfde5b665fad75b81b74af5101e901b12db4d
Dobrowydka <andrikchannelyou@gmail.com>
2022-04-06 12:52:17 +0300

    CommandLineRunner

--- src/main/java/by/library/yurueu/Runner.java
+++ src/main/java/by/library/yurueu/Runner.java
@@ -1,11 +1,22 @@
 package by.library.yurueu;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Runner {
+public class Runner implements CommandLineRunner {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(Runner.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
     }
 }
