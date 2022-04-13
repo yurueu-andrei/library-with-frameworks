b1f28e350359bc210c578b12127589acddd3bac1
Dobrowydka <andrikchannelyou@gmail.com>
2022-04-13 17:35:15 +0300

    refactoring genre controller

--- src/main/java/by/library/yurueu/controller/GenreController.java
+++ src/main/java/by/library/yurueu/controller/GenreController.java
@@ -1,17 +1,11 @@
 package by.library.yurueu.controller;
 
-import by.library.yurueu.dto.GenreListDto;
-import by.library.yurueu.dto.GenreSaveDto;
-import by.library.yurueu.dto.GenreUpdateDto;
+import by.library.yurueu.dto.GenreDto;
 import by.library.yurueu.exception.ServiceException;
 import by.library.yurueu.service.GenreService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,28 +18,12 @@ public class GenreController {
     private final GenreService genreService;
 
     @GetMapping("/{id}")
-    public GenreListDto findById(@PathVariable Long id) throws ServiceException {
+    public GenreDto findById(@PathVariable Long id) throws ServiceException {
         return genreService.findById(id);
     }
 
     @GetMapping
-    public List<GenreListDto> findAll() throws ServiceException {
+    public List<GenreDto> findAll() throws ServiceException {
         return genreService.findAll();
-    }
-
-    @PostMapping
-    public GenreSaveDto add(@RequestBody GenreSaveDto genreSaveDto) throws ServiceException {
-        return genreService.add(genreSaveDto);
-    }
-
-    @PutMapping
-    public GenreUpdateDto update(@RequestBody GenreUpdateDto genreUpdateDto) throws ServiceException {
-        genreService.update(genreUpdateDto);
-        return genreUpdateDto;
-    }
-
-    @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable Long id) throws ServiceException {
-        return genreService.delete(id);
     }
 }
