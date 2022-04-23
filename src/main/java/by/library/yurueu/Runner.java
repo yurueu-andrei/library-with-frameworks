2a9f1d33cd261143f6494b4ce30bf3a603476aa0
Dobrowydka <andrikchannelyou@gmail.com>
2022-04-23 13:20:02 +0300

    runner

--- src/main/java/by/library/yurueu/Runner.java
+++ src/main/java/by/library/yurueu/Runner.java
@@ -17,10 +17,6 @@ public class Runner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        UserService bean = applicationContext.getBean(UserService.class);
-//        OrderService bean2 = applicationContext.getBean(OrderService.class);
-//        System.out.println(bean2.findAll());
-//        bean.delete(1L);
-//        System.out.println(bean2.findAll());
+
     }
 }
