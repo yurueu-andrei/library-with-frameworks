1410bdc9d25197315bcd295140b15171522a628e
Dobrowydka <andrikchannelyou@gmail.com>
2022-05-03 23:04:26 +0300

    Runner

--- src/main/java/by/library/yurueu/Runner.java
+++ src/main/java/by/library/yurueu/Runner.java
@@ -1,36 +1,13 @@
 package by.library.yurueu;
 
-import by.library.yurueu.mapper.AuthorMapper;
-import by.library.yurueu.repository.AuthorRepository;
-import by.library.yurueu.service.AuthorService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 @Profile("!test")
-public class Runner implements CommandLineRunner {
-    @Autowired
-    private ApplicationContext applicationContext;
-    @Autowired
-    private AuthorRepository authorRepository;
-    @Autowired
-    private AuthorService authorService;
-    @Autowired
-    private AuthorMapper authorMapper;
-
+public class Runner {
     public static void main(String[] args) {
         SpringApplication.run(Runner.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        authorRepository.findAll();
-        authorRepository.findById(3L);
     }
 }
